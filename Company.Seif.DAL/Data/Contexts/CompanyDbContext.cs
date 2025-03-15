1146d0b3d426de8339e18d7f3245c63ac7fb16aa
Greg Low <greg@gmail.com>
2025-03-16 00:16:15 +0200

    Implementaion of DAl

---
+++ Company.Seif.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Company.Seif.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Seif.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; Database = CompanyGO2; Trusted_Connection = True ; TrustedServerCertificate = True ");
+        }
+        public DbSet<Department> Departments { get; set; }
+        public object Assemply { get; private set; }
+    }
+}
+
