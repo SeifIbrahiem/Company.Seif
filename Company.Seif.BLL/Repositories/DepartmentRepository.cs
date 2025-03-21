0a1c68ea6e69e09895a3ffb413e2525bd9c422d7
Greg Low <greg@gmail.com>
2025-03-21 14:07:10 +0200

    Refactoring To use Generic RepositoryDP

--- Company.Seif.BLL/Repositories/DepartmentRepository.cs
+++ Company.Seif.BLL/Repositories/DepartmentRepository.cs
@@ -10,40 +10,12 @@ using System.Threading.Tasks;
 
 namespace Company.Seif.BLL.Repositories
 {
-    public class DepartmentRepository : IDepartmentRepository
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
     {
-        private readonly CompanyDbContext _context; //null
-        
-        //ASK CLR create object from companyDbcontext
-        public DepartmentRepository(CompanyDbContext context) 
-        {
-            _context = context;
-        }
-        public IEnumerable<Department> GetAll()
-        {
-            return _context.Departments.ToList();
-        }
+       public DepartmentRepository(CompanyDbContext context) : base(context)//ask clr create object from CompanyDbContext
 
-        public Department? Get(int id)
         {
-            return _context.Departments.Find(id);
-        }
-        public int Add(Department model)
-        {
-            _context.Departments.Add(model);
-            return _context.SaveChanges();
-        }
 
-        public int Delete(Department model)
-        {
-            _context.Departments.Remove(model);
-            return _context.SaveChanges();
-        }
-
-        public int Update(Department model)
-        {
-            _context.Departments.Update(model);
-            return _context.SaveChanges();
         }
     }
 }
