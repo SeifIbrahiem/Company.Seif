005c8315d4da9f132006619638e207554ea5dd54
Greg Low <greg@gmail.com>
2025-04-16 22:27:47 +0200

    Secuirtymodule add acount and add user

---
+++ Company.Seif.PL/Helbers/EmailSetting.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Company.Seif.PL.Helbers
+{
+	public static class EmailSetting
+	{
+		public static bool SendEmail (Email email)
+		{
+			//Mail server 
+			//SMTB
+
+			try
+			{
+				var client = new SmtpClient("smtp.gmail.com", 587);
+				client.EnableSsl = true;
+				client.Credentials = new NetworkCredential("elslimanysief@gmail.com", "gujfcazxnjnkcvrc"); //sender
+				client.Send("elslimanysief@gmail.com", email.To, email.Subject, email.Body);
+				//gujfcazxnjnkcvrc
+				return true;
+			}
+			catch (Exception e) 
+			{ 
+			 return false;
+			}
+		}
+	}
+}
